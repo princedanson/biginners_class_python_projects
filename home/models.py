867414931dab0aac999564da54fc212a9cf8fcf7
Nonso <41114094+princedanson@users.noreply.github.com>
2026-08-25 20:03:40 +0100

    Update models.py

--- home/models.py
+++ home/models.py
@@ -3,5 +3,5 @@ from django.db import models
 # This model stores baby names in the database.
 # Each record will have a single text field called 'name'.
 class baby_names(models.Model):
-    name = models.CharField(max_length=255, unique=True, null=False, blank=False)
+    name = models.CharField(max_length=255)
 
